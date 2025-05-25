8a454e2dde3f1a5885e5e0455f50b549d20104db
Pranav Saxena <pranavsaxena.ramanujan@gmail.com>
2025-05-25 10:14:35 +0530

    fix

--- middleware/data/src/main/java/in/ramanujan/data/db/impl/VariableValueDao/VariableValueSqlDbImpl.java
+++ middleware/data/src/main/java/in/ramanujan/data/db/impl/VariableValueDao/VariableValueSqlDbImpl.java
@@ -136,7 +136,7 @@ public class VariableValueSqlDbImpl implements VariableValueDao {
         try {
             ArrayMapping arrayMapping = new ArrayMapping();
             arrayMapping.setAsyncId(asyncId);
-            arrayMapping.setArrayName(arrayName);
+            arrayMapping.setArrayName("");
             arrayMapping.setArrayId(arrayId);
             arrayMapping.setIndexStr(index);
             if(value != null) {
